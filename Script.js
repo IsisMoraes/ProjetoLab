98a0035ab693c1418f354f00408dda15e9f9b8ef
Isis Moraes <51097629+IsisMoraes@users.noreply.github.com>
2020-06-05 22:40:56 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -11,8 +11,6 @@ window.onload = () => {
 }
 
 //botao escolhido no html
-
-//Aqui estamos pegando por id o botão ja existente no html e declarando ele como uma variavel constante no JavaScript
 const botao = document.getElementById("botaoEscolhido");
 
 const botaoDir = document.getElementById("setaDireita");
@@ -21,36 +19,97 @@ const botaoEsq = document.getElementById("setaEsquerda");
 
 const botaoMudaEsq = document.getElementById("tras");
 
-// funcao botao ir para ir para rotacionar
+const botaoMudaDir = document.getElementById("frente");
+
+// funcao botao ir para ir para direita
+
 function rotacionar() {
-  peca1.object3D.rotation.y += 0.1;
+  var x = document.getElementsByClassName("peca");
+  for(i=0;i<4;i++){
+    x[i].object3D.rotation.y += 0.5;
+  }  
 }
 
-// funcao botao ir para ir para Direita
 function andarPraDireita() {
-  peca1.object3D.position.x += 0.1;
+  var x = document.getElementsByClassName("peca");
+  for(i=0;i<4;i++){
+    x[i].object3D.position.x += 0.5;
+  }  
 }
 
-// funcao botao ir para ir para Esquerda
 function andarPraEsquerda(){
-  peca1.object3D.position.x -= 0.1;
+  var x = document.getElementsByClassName("peca");
+  for(i=0;i<4;i++){
+    x[i].object3D.position.x -= 0.5;
+  }  
 }
 
-//Mudar a peca que esta visivel
-// Cria variaveis locais de acordo com o id de cada peca e faz a verificacao se o atributo visible da peca ta true e muda pra false
-// Tem que fazer isso pra todas as 4 pecas
-function mudaPeca() {
-	var p1 = document.getElementById("peca1");
+//Mudar a peca esquerda
+function mudaPecaEsquerda() {
+  var x = document.getElementsByClassName("peca");
+  for(i=0; i<4; i++){
+    var j = 1;
+
+    if(x[i].object3D.visible == true){
+      
+      x[i].setAttribute('visible', false); 
+      x[j].setAttribute('visible', true);
+      
+    }
+    j++;
+  }
+  
+  
+}
+/* 
+  var p1 = document.getElementById("peca1");
 	var p2 = document.getElementById("peca2");
+  var p3 = document.getElementById("peca3");
+  var p4 = document.getElementById("peca4");
     if (p1.object3D.visible == true) {
        p1.setAttribute('visible', false); 
        p2.setAttribute('visible', true); 
-       }
- }
-
-//adicionando evento de click
-//Aqui estamos adicionando ao nosso botao uma lista de eventos... por enquanto so estamos vazendo um evento "Click" que vai chamar a funcao que criamos
+ 	}
+  else if(p2.object3D.visible == true){
+       p2.setAttribute('visible', false); 
+       p3.setAttribute('visible', true); 
+  }
+   else if(p3.object3D.visible == true){
+       p3.setAttribute('visible', false); 
+       p4.setAttribute('visible', true); 
+  }
+     else if(p4.object3D.visible == true){
+       p4.setAttribute('visible', false); 
+       p1.setAttribute('visible', true); 
+  }
+}
+*/
+//Mudar a peca direita
+function mudaPecaDireita() {
+	var p1 = document.getElementById("peca1");
+	var p2 = document.getElementById("peca2");
+  var p3 = document.getElementById("peca3");
+  var p4 = document.getElementById("peca4");
+    if (p1.object3D.visible == true) {
+       p1.setAttribute('visible', false); 
+       p4.setAttribute('visible', true); 
+ 	}
+  else if(p2.object3D.visible == true){
+       p2.setAttribute('visible', false); 
+       p1.setAttribute('visible', true); 
+  }
+   else if(p3.object3D.visible == true){
+       p3.setAttribute('visible', false); 
+       p2.setAttribute('visible', true); 
+  }
+     else if(p4.object3D.visible == true){
+       p4.setAttribute('visible', false); 
+       p3.setAttribute('visible', true); 
+  }
+}
+//adicionando evento de click, com a função andarParaDireita feita acima
 botao.addEventListener("click", rotacionar);
 botaoDir.addEventListener("click", andarPraDireita);
 botaoEsq.addEventListener("click", andarPraEsquerda);
-botaoMudaEsq.addEventListener("click", mudaPeca);
+botaoMudaEsq.addEventListener("click", mudaPecaEsquerda);
+botaoMudaDir.addEventListener("click", mudaPecaDireita);
